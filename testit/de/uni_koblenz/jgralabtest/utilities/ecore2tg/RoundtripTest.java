41216d7cb62a827d1f7b294278c57b7c1680f775
Kristina Heckelmann <kheckelmann@uni-koblenz.de>
2012-06-13 12:22:52 +0200

    add testdata folder and adapt Roundtrip test

--- testit/de/uni_koblenz/jgralabtest/utilities/ecore2tg/RoundtripTest.java
+++ testit/de/uni_koblenz/jgralabtest/utilities/ecore2tg/RoundtripTest.java
@@ -1,27 +1,28 @@
 package de.uni_koblenz.jgralabtest.utilities.ecore2tg;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.xml.stream.XMLStreamException;
 
 import org.eclipse.emf.ecore.EObject;
 
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphIO;
 import de.uni_koblenz.jgralab.GraphIOException;
-import de.uni_koblenz.jgralab.codegenerator.CodeGeneratorConfiguration;
 import de.uni_koblenz.jgralab.grumlschema.SchemaGraph;
 import de.uni_koblenz.jgralab.schema.Schema;
 import de.uni_koblenz.jgralab.utilities.ecore2tg.Ecore2Tg;
-import de.uni_koblenz.jgralab.utilities.ecore2tg.Tg2Ecore;
 import de.uni_koblenz.jgralab.utilities.ecore2tg.Ecore2TgConfiguration.TransformParams;
-import de.uni_koblenz.jgralab.utilities.rsa.SchemaGraph2XMI;
+import de.uni_koblenz.jgralab.utilities.ecore2tg.Tg2Ecore;
 import de.uni_koblenz.jgralab.utilities.tg2dot.Tg2Dot;
+import de.uni_koblenz.jgralab.utilities.tg2dot.dot.GraphVizOutputFormat;
 import de.uni_koblenz.jgralab.utilities.tg2schemagraph.Schema2SchemaGraph;
 import de.uni_koblenz.jgralab.utilities.tg2schemagraph.SchemaGraph2Schema;
 
 public class RoundtripTest {
+
+	public static final String testfolder = "testit" + File.separator
+			+ "testdata" + File.separator;
 
 	/**
 	 * @param args
@@ -42,11 +43,19 @@ public class RoundtripTest {
 		System.out.println("#########################");
 		System.out.println("Testcase: ExtendedLibrary");
 
-		String graph_filename = "../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeckGraph.tg";
-		String metamodel_filename = "../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeck_metamodel.ecore";
-		String model_filename = "../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeck";
-		String graph_back_filename = "../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeck_back.tg";
-		String dot_filename = "../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeck.dot";
+		String folder = testfolder + "tg2ecore2tg_library" + File.separator;
+		String genfolder = folder + "gen" + File.separator;
+		// IN
+		String graph_filename = folder + "library_seck_graph.tg";
+		String config_filename = folder + "config.plist";
+
+		// OUT Tg2Ecore
+		String metamodel_filename = genfolder + "library_metamodel.ecore";
+		String model_filename = genfolder + "library_seck_model";
+
+		// OUT Ecore2TG
+		String graph_back_filename = genfolder + "library_seck_graph_back.tg";
+		String graphPicture_filename = genfolder + "libary_seck_graph_back.png";
 
 		// --------------------------------------------
 		// --- Tg -> Ecore ----------------------------
@@ -55,29 +64,18 @@ public class RoundtripTest {
 		System.out.println("\nTg -> Ecore");
 
 		Schema schema = GraphIO.loadSchemaFromFile(graph_filename);
-		schema.compile(CodeGeneratorConfiguration.MINIMAL);
-		schema.commit("../../GeneratedTest/",
-				CodeGeneratorConfiguration.MINIMAL);
+
 		Schema2SchemaGraph s2sg = new Schema2SchemaGraph();
 		SchemaGraph sg = s2sg.convert2SchemaGraph(schema);
 
-		SchemaGraph2XMI sg2xmi = new SchemaGraph2XMI();
-		try {
-			sg2xmi.process(sg,
-					"../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeckSchema.xmi");
-		} catch (XMLStreamException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		Tg2Ecore tg2ec = new Tg2Ecore(sg);
-		tg2ec.fillWithConfigurationsFromFile("../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/config.plist");
+		tg2ec.fillWithConfigurationsFromFile(config_filename);
 		tg2ec.transform();
 		tg2ec.saveEcoreMetamodel(tg2ec.getTransformedMetamodel(),
 				metamodel_filename);
 
 		Graph g = GraphIO.loadGraphFromFile(graph_filename, null);
-		Tg2Dot.convertGraph(g, dot_filename);
+		Tg2Dot.convertGraph(g, graphPicture_filename, GraphVizOutputFormat.PNG);
 
 		ArrayList<EObject> model = tg2ec.transformGraphToEcoreModel(g);
 		tg2ec.saveEcoreModel(model, model_filename);
@@ -100,15 +98,6 @@ public class RoundtripTest {
 				+ tg2ec.getOption_rootpackageName() });
 		GraphIO.saveGraphToFile(gr, graph_back_filename, null);
 
-		sg2xmi = new SchemaGraph2XMI();
-		try {
-			sg2xmi.process(s2sg.convert2SchemaGraph(sg2s.convert(ec2tg
-					.getSchemaGraph())),
-					"../../freshEcore2Tg/ecore2tg/tests/Tg2EcoreRoundTest/BibSeckSchema_back.xmi");
-		} catch (XMLStreamException e) {
-			e.printStackTrace();
-		}
-
 		System.out.println();
 	}
 
@@ -117,12 +106,20 @@ public class RoundtripTest {
 		System.out.println("Testcase: ExtendedUniversity");
 
 		// Filenames
-		String metamodel_filename = "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/UniversityExtended.ecore";
-		String[] model_filename = { "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/Model01.universityextended" };
-		String graph_filename = "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/UniversityExtended_withModel.tg";
-		String graph_dot_filename = "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/UniversityExtended.dot";
-		String metamodel_back_filename = "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/UniversityExtended_back.ecore";
-		String model_back_filename = "../../freshEcore2Tg/ecore2tg/tests/UniversityExtended/Model01_back";
+		String folder = testfolder + "ecore2tg2ecore_university"
+				+ File.separator;
+		String genfolder = folder + "gen" + File.separator;
+
+		// IN
+		String metamodel_filename = folder + "UniversityExtended.ecore";
+		String[] model_filename = { folder + "Model01.universityextended" };
+
+		// OUT
+		String graph_filename = genfolder + "UniversityExtended_graph.tg";
+		String graph_dot_filename = genfolder + "UniversityExtended_graph.png";
+		String metamodel_back_filename = genfolder
+				+ "UniversityExtended_back.ecore";
+		String model_back_filename = genfolder + "Model01_back";
 
 		// --------------------------------------------
 		// --- Ecore -> Tg ----------------------------
@@ -134,12 +131,9 @@ public class RoundtripTest {
 		ec2tg.getConfiguration().setTransformationOption(
 				TransformParams.AUTOMATIC_TRANSFORMATION);
 		ec2tg.transform("org.test.ExtendedUniversity");
-		SchemaGraph2Schema sg2s_2 = new SchemaGraph2Schema();
-		GraphIO.saveSchemaToFile(sg2s_2.convert(ec2tg.getSchemaGraph()),
-				graph_filename);
 		Graph graph = ec2tg.transformModel(model_filename);
 		GraphIO.saveGraphToFile(graph, graph_filename, null);
-		Tg2Dot.convertGraph(graph, graph_dot_filename);
+		Tg2Dot.convertGraph(graph, graph_dot_filename, GraphVizOutputFormat.PNG);
 
 		// --------------------------------------------
 		// --- Tg -> Ecore ----------------------------
