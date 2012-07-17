7489722aa3fcf23262e83dcf610a7010afaccb8e
Kristina Heckelmann <kheckelmann@uni-koblenz.de>
2012-07-17 19:39:07 +0200

    add more real tests

--- testit/de/uni_koblenz/jgralabtest/utilities/ecore2tg/Ecore2TgTest.java
+++ testit/de/uni_koblenz/jgralabtest/utilities/ecore2tg/Ecore2TgTest.java
@@ -1,15 +1,20 @@
 package de.uni_koblenz.jgralabtest.utilities.ecore2tg;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Iterator;
 
 import org.junit.Test;
 
+import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphIO;
 import de.uni_koblenz.jgralab.GraphIOException;
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.schema.AggregationKind;
 import de.uni_koblenz.jgralab.schema.EdgeClass;
 import de.uni_koblenz.jgralab.schema.Schema;
 import de.uni_koblenz.jgralab.utilities.ecore2tg.Ecore2Tg;
@@ -225,5 +230,166 @@ public class Ecore2TgTest {
 
 		String tgfile2 = basefile + "gen" + File.separator + name + "2.tg";
 		g2.save(tgfile2);
+	}
+
+	@Test
+	public void testUserManagement() throws GraphIOException {
+		String name = "UserManagement";
+		String basefile = testdataFolder + name + File.separator;
+		String ecorefile = basefile + name + ".ecore";
+
+		// Transformation
+		Ecore2Tg test = new Ecore2Tg(ecorefile);
+		Ecore2TgConfiguration conf = test.getConfiguration();
+
+		conf.getNamesOfEdgeClassesMap().put(
+				"usermanagement.Society.publishedPaper", "Publishes");
+		conf.getNamesOfEdgeClassesMap().put("usermanagement.User.writtenPaper",
+				"Writes");
+
+		conf.setTransformationOption(TransformParams.AUTOMATIC_TRANSFORMATION);
+		test.transform("test.Usermanagement");
+
+		Schema schema = test.getSchema();
+
+		assertTrue(schema.getAttributedElementClass("EntranceIntoGroup") instanceof EdgeClass);
+		assertTrue(schema.getGraphClass().getEdgeClass("EntranceIntoGroup")
+				.getTo().getAggregationKind().equals(AggregationKind.COMPOSITE));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("Publishes") != null);
+		assertTrue(schema.getGraphClass().getEdgeClass("Publishes").getFrom()
+				.getVertexClass().getQualifiedName().equals("Society"));
+		assertTrue(schema.getGraphClass().getEdgeClass("Publishes").getTo()
+				.getVertexClass().getQualifiedName().equals("Paper"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("Writes") != null);
+		assertTrue(schema.getGraphClass().getEdgeClass("Writes").getFrom()
+				.getVertexClass().getQualifiedName().equals("User"));
+		assertTrue(schema.getGraphClass().getEdgeClass("Writes").getTo()
+				.getVertexClass().getQualifiedName().equals("Paper"));
+
+		String modelpath = basefile + "Example1.usermanagement";
+		Graph g = test.transformModel(new String[] { modelpath });
+
+		Iterator<Vertex> socIt = g.vertices(
+				schema.getGraphClass().getVertexClass("Society")).iterator();
+		assertTrue(socIt.hasNext());
+		socIt.next();
+		assertFalse(socIt.hasNext());
+
+		String tgfilename = basefile + "gen" + File.separator + name + ".tg";
+		g.save(tgfilename);
+
+	}
+
+	@Test
+	public void testSimpleNodeAndWay() throws GraphIOException {
+		String name = "SimpleNodeAndWay";
+		String basefile = testdataFolder + name + File.separator;
+		String ecorefile = basefile + name + ".ecore";
+
+		// Transformation
+		Ecore2Tg test = new Ecore2Tg(ecorefile);
+		Ecore2TgConfiguration conf = test.getConfiguration();
+
+		conf.getDirectionMap().put("simplenodeandway.Way.endOfWay",
+				Ecore2TgConfiguration.TO);
+
+		conf.setTransformationOption(TransformParams.AUTOMATIC_TRANSFORMATION);
+		test.transform("test.SimpleNodeAndWay");
+
+		Schema schema = test.getSchema();
+
+		assertTrue(schema.getAttributedElementClass("Way") instanceof EdgeClass);
+		assertTrue(schema.getGraphClass().getVertexClass("Node") != null);
+
+		String modelpath = basefile + "Example1.simplenodeandway";
+		Graph g = test.transformModel(new String[] { modelpath });
+
+		Edge e = g.getFirstEdge();
+		assertEquals(1.0, e.getOmega().getAttribute("latitude"));
+
+		String tgfilename = basefile + "gen" + File.separator + name + ".tg";
+		g.save(tgfilename);
+
+	}
+
+	@Test
+	public void testSpecialCases() throws GraphIOException {
+		String name = "SpecialCases";
+		String basefile = testdataFolder + name + File.separator;
+		String ecorefile = basefile + name + ".ecore";
+
+		// Transformation
+		Ecore2Tg test = new Ecore2Tg(ecorefile);
+		Ecore2TgConfiguration conf = test.getConfiguration();
+
+		conf.getEdgeClassesList().add("SpecialCases.EdgeAB");
+		conf.getEdgeClassesList().add("SpecialCases.EdgeEF");
+		conf.getEdgeClassesList().add("SpecialCases.EdgeGH");
+		conf.getEdgeClassesList().add("SpecialCases.EdgeIJ");
+
+		// Start Transformation
+		conf.setTransformationOption(TransformParams.AUTOMATIC_TRANSFORMATION);
+		test.transform("test.SpecialCases");
+
+		Schema schema = test.getSchema();
+
+		assertTrue(schema.getAttributedElementClass("EdgeAB") instanceof EdgeClass);
+		assertTrue(schema.getAttributedElementClass("EdgeCD") instanceof EdgeClass);
+		assertTrue(schema.getAttributedElementClass("EdgeEF") instanceof EdgeClass);
+		assertTrue(schema.getAttributedElementClass("EdgeGH") instanceof EdgeClass);
+		assertTrue(schema.getAttributedElementClass("EdgeIJ") instanceof EdgeClass);
+		assertTrue(schema.getAttributedElementClass("EdgeKL") instanceof EdgeClass);
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeAB").getFrom()
+				.getVertexClass().getQualifiedName().equals("A"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeAB").getTo()
+				.getVertexClass().getQualifiedName().equals("B"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeCD").getFrom()
+				.getVertexClass().getQualifiedName().equals("C"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeCD").getTo()
+				.getVertexClass().getQualifiedName().equals("D"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeEF").getFrom()
+				.getVertexClass().getQualifiedName().equals("E"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeEF").getTo()
+				.getVertexClass().getQualifiedName().equals("F"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeGH").getFrom()
+				.getVertexClass().getQualifiedName().equals("G"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeGH").getTo()
+				.getVertexClass().getQualifiedName().equals("H"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeIJ").getFrom()
+				.getVertexClass().getQualifiedName().equals("I"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeIJ").getTo()
+				.getVertexClass().getQualifiedName().equals("J"));
+
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeKL").getFrom()
+				.getVertexClass().getQualifiedName().equals("K"));
+		assertTrue(schema.getGraphClass().getEdgeClass("EdgeKL").getTo()
+				.getVertexClass().getQualifiedName().equals("L"));
+
+		assertEquals(AggregationKind.NONE,
+				schema.getGraphClass().getEdgeClass("EdgeAB").getTo()
+						.getAggregationKind());
+		assertEquals(AggregationKind.NONE,
+				schema.getGraphClass().getEdgeClass("EdgeCD").getTo()
+						.getAggregationKind());
+		assertEquals(AggregationKind.COMPOSITE, schema.getGraphClass()
+				.getEdgeClass("EdgeEF").getTo().getAggregationKind());
+		assertEquals(AggregationKind.NONE,
+				schema.getGraphClass().getEdgeClass("EdgeGH").getTo()
+						.getAggregationKind());
+		assertEquals(AggregationKind.NONE,
+				schema.getGraphClass().getEdgeClass("EdgeIJ").getTo()
+						.getAggregationKind());
+		assertEquals(AggregationKind.NONE,
+				schema.getGraphClass().getEdgeClass("EdgeKL").getTo()
+						.getAggregationKind());
+		String tgfile = basefile + "gen" + File.separator + name + ".tg";
+		schema.save(tgfile);
 	}
 }
